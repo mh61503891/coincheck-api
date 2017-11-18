382a04f35c0eb0db15be3787f8482bcb0659729e
Masayuki Higashino <msyk@hgsn.info>
2017-11-18 19:13:58 +0900

    Run bundle gem coincheck-api bundle gem coincheck-api --no-coc --mit --exe --test=rspec

---
+++ lib/coincheck/api.rb
@@ -0,0 +1,7 @@
+require "coincheck/api/version"
+
+module Coincheck
+  module Api
+    # Your code goes here...
+  end
+end
---
+++ spec/coincheck/api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Coincheck::Api do
+  it "has a version number" do
+    expect(Coincheck::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
