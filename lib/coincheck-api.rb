d2ac2517146360f4155b5df5b0adede992519952
Masayuki Higashino <msyk@hgsn.info>
2017-11-19 12:14:14 +0900

    Support Public API

--- lib/coincheck-api.rb
+++ lib/coincheck-api.rb
@@ -2,25 +2,50 @@ require 'httpclient'
 require 'uri'
 require 'openssl'
 require 'json'
+require 'active_support/core_ext/hash/compact'
 
 module Coincheck
   class API
 
     VERSION = '0.1.0'
 
-    def initialize(access_key, secret_key)
+    def initialize(access_key='', secret_key='')
       @access_key = access_key
       @secret_key = secret_key
     end
 
     def ticker
-      client = Client.new(@access_key, @secret_key, '/api/ticker')
-      client.get('/api/ticker')
+      Client.new(@access_key, @secret_key, '/api/ticker').get
+    end
+
+    def trades(offset:nil)
+      params = {
+        offset:offset
+      }.compact
+      Client.new(@access_key, @secret_key, '/api/trades', params).get
+    end
+
+    def order_books
+      Client.new(@access_key, @secret_key, '/api/order_books').get
+    end
+
+    def exchange_orders_rate(order_type:, pair:, amount:nil, price:nil)
+      params = {
+        order_type:order_type,
+        pair:pair,
+        amount:amount,
+        price:price
+      }.compact
+      Client.new(@access_key, @secret_key, '/api/exchange/orders/rate', params).get
+    end
+
+    def rate(pair:)
+      Client.new(@access_key, @secret_key, "/api/rate/#{pair}").get
     end
 
     class Client
 
-      def initialize(access_key, secret_key, path='', params={})
+      def initialize(access_key='', secret_key='', path='', params={})
         @access_key = access_key
         @secret_key = secret_key
         @path = path
@@ -30,15 +55,37 @@ module Coincheck
       def get
         http = HTTPClient.new
         response = http.get(uri, query, header.to_a)
-        JSON.parse(response.body)
+        return JSON.parse(response.body) if response.ok?
+        raise response.reason
       end
 
       def uri
-        @uri ||= URI.parse("https://#{host}#{@path}")
+        @uri ||= URI.parse("https://#{host}#{path}")
       end
 
       def host
         'coincheck.com'
+      end
+
+      def path
+        @path
+      end
+
+      def query
+        @params
+      end
+
+      def header
+        return {} if @access_key.empty? || @secret_key.empty?
+        key = @access_key
+        nonce = Time.now.to_i.to_s
+        message = nonce + uri.to_s + URI.encode_www_form(query)
+        signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), @secret_key, message)
+        {
+          'ACCESS-KEY' => key,
+          'ACCESS-NONCE' => nonce,
+          'ACCESS-SIGNATURE' => signature
+        }
       end
 
     end
--- spec/coincheck/api_spec.rb
+++ spec/coincheck/api_spec.rb
@@ -1,5 +1,36 @@
 RSpec.describe Coincheck::API do
-  it 'has a version number' do
+
+  it '#version' do
     expect(Coincheck::API::VERSION).not_to be nil
   end
+
+  let(:api) { Coincheck::API.new }
+
+  it '#ticker' do
+    api.ticker
+  end
+
+  it '#trades' do
+    api.trades(offset:50)
+  end
+
+  it '#order_books' do
+    api.order_books
+  end
+
+  it '#exchange_orders_rate' do
+    api.exchange_orders_rate(order_type:'sell', pair:'btc_jpy', amount:0.1)
+    api.exchange_orders_rate(order_type:'sell', pair:'btc_jpy', price:28000)
+    expect {
+      api.exchange_orders_rate(order_type:'sell', pair:'btc_jpy')
+    }.to raise_error(RuntimeError, 'Bad Request')
+    expect {
+      api.exchange_orders_rate(order_type:'sell', pair:'btc_jpy', amount:0.1, price:28000)
+    }.to raise_error(RuntimeError, 'Bad Request')
+  end
+
+  it '#rate' do
+    api.rate(pair:'btc_jpy')
+  end
+
 end
